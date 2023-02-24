057fc4cb06711ae9696827198152689a533af806
Randy Pagels <rpagels@microsoft.com>
2023-02-24 14:53:16 -0500

    Added OpenAIController

---
+++ AzureOpenAIDemo.Api/Controllers/OpenAIController.cs
@@ -0,0 +1,102 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureOpenAIDemo.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OpenAIController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey = "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";
+    private const string _baseUrl = "https://api.openai.com/v1";
+
+    public OpenAIController(HttpClient httpClient, string apiKey)
+    {
+        _httpClient = httpClient;
+        _apiKey = apiKey;
+    }
+
+    [HttpGet(Name = "GetCodeDavinci002")]
+    public async Task<string> GetCodeDavinci002()
+    {
+        //var modelEndpoint = "/engines/text-davinci-002/completions";
+        var modelEndpoint = "/completions";
+        var requestBody = new
+        {
+            prompt = "Write a program in C# to reverse a string",
+            temperature = 0.5,
+            max_tokens = 60,
+            n = 1,
+            stop = "\n",
+            model = "text-davinci-002"
+        };
+
+        var response = await SendCompletionRequest(modelEndpoint, requestBody);
+        var choices = response?.choices;
+        var completionText = choices?[0]?.text;
+
+        return completionText;
+    }
+
+    //[HttpGet(Name = "GetCodeDavinci003")]
+    //public async Task<string> GetCodeDavinci003()
+    //{
+    //    //var modelEndpoint = "/engines/text-davinci-002/completions";
+    //    var modelEndpoint = "/completions";
+    //    var requestBody = new
+    //    {
+    //        prompt = "Write a program in C# to find the largest number in an array",
+    //        max_tokens = 60,
+    //        n = 1,
+    //        stop = "\n",
+    //        model = "text-davinci-003"
+    //    };
+
+    //    var response = await SendCompletionRequest(modelEndpoint, requestBody);
+    //    var choices = response?.choices;
+    //    var completionText = choices?[0]?.text;
+
+    //    return completionText;
+    //}
+
+    //[HttpGet(Name = "GetCodeDavinci003")]
+    //public async Task<string> GetCodeDavinci003()
+    //{
+    //    //var modelEndpoint = "/engines/text-davinci-002/completions";
+    //    var modelEndpoint = "/completions";
+    //    var requestBody = new
+    //    {
+    //        prompt = "Write a program in C# to find the largest number in an array",
+    //        max_tokens = 60,
+    //        n = 1,
+    //        stop = "\n",
+    //        model = "text-davinci-003"
+    //    };
+
+    //    var response = await SendCompletionRequest(modelEndpoint, requestBody);
+    //    var choices = response?.choices;
+    //    var completionText = choices?[0]?.text;
+
+    //    return completionText;
+    //}
+    
+    private async Task<dynamic> SendCompletionRequest(string modelEndpoint, dynamic requestBody)
+    {
+        var requestUrl = _baseUrl + modelEndpoint;
+        var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey);
+        request.Content = new StringContent(System.Text.Json.JsonSerializer.Serialize(requestBody), System.Text.Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseData = System.Text.Json.JsonSerializer.Deserialize<dynamic>(responseContent);
+            return responseData?.choices;
+        }
+
+        return null;
+    }
+}
--- AzureOpenAIDemo.Api/Controllers/WeatherForecastController.cs
+++ AzureOpenAIDemo.Api/Controllers/WeatherForecastController.cs
@@ -1,65 +1,43 @@
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
-namespace ChatGPTDemo.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private HttpClient _httpClient;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+//using System.Net.Http;
+//using System.Text;
+//namespace ChatGPTDemo.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class WeatherForecastController : ControllerBase
+//    {
+//        private HttpClient _httpClient;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-        
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+//        private readonly ILogger<WeatherForecastController> _logger;
 
-        [HttpGet(Name = "GetTextDavinci003")]
-        public async Task<string> GetTextDavinci003()
-        {
-            var json = JsonConvert.SerializeObject(new
-            {
-                prompt = "What is the meaning of life",
-                temperature = 0.5,
-                max_tokens = 50,
-                model = "text-davinci-003"
-            });
-            
-            _httpClient = new HttpClient();
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "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");
-            //var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci/completions", content);
-            var response = await _httpClient.PostAsync(" https://api.openai.com/v1/completions", content);
-            var responseJson = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<dynamic>(responseJson);
-            
-            return result!.choices[0].text;
-        }
+//        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//        {
+//            _logger = logger;
+//        }
 
-        [HttpGet(Name = "GetCodeDavinci002")]
-        public async Task<string> GetCodeDavinci002()
-        {
-            var json = JsonConvert.SerializeObject(new
-            {
-                prompt = "What is the meaning of life",
-                temperature = 0.5,
-                max_tokens = 50,
-                model = "code-davinci-002"
-            });
+//        [HttpGet(Name = "GetTextDavinci003")]
+//        public async Task<string> Get()
+//        {
+//            var json = JsonConvert.SerializeObject(new
+//            {
+//                prompt = "What is the meaning of life",
+//                temperature = 0.5,
+//                max_tokens = 50,
+//                model = "text-davinci-003"
+//            });
 
-            _httpClient = new HttpClient();
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "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");
-            //var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci/completions", content);
-            var response = await _httpClient.PostAsync(" https://api.openai.com/v1/completions", content);
-            var responseJson = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<dynamic>(responseJson);
+//            _httpClient = new HttpClient();
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "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");
+//            //var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci/completions", content);
+//            var response = await _httpClient.PostAsync(" https://api.openai.com/v1/completions", content);
+//            var responseJson = await response.Content.ReadAsStringAsync();
+//            var result = JsonConvert.DeserializeObject<dynamic>(responseJson);
 
-            return result!.choices[0].text;
-        }
+//            return result!.choices[0].text;
+//        }
 
-    }
-}
+//    }
+//}
