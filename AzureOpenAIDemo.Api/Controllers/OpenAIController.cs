5f9f76876753c51168af84de5e87469d4043a149
Randy Pagels <rpagels@microsoft.com>
2023-02-24 16:46:36 -0500

    Updates

--- AzureOpenAIDemo.Api/Controllers/OpenAIController.cs
+++ AzureOpenAIDemo.Api/Controllers/OpenAIController.cs
@@ -9,7 +9,7 @@ namespace AzureOpenAIDemo.Api.Controllers;
 public class OpenAIController : ControllerBase
 {
     private readonly HttpClient _httpClient;
-    private readonly string _apiKey = "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";
+    private readonly string _apiKey = "";
     private const string _baseUrl = "https://api.openai.com/v1";
 
     public OpenAIController(HttpClient httpClient, string apiKey)
@@ -40,47 +40,26 @@ public class OpenAIController : ControllerBase
         return completionText;
     }
 
-    //[HttpGet(Name = "GetCodeDavinci003")]
-    //public async Task<string> GetCodeDavinci003()
-    //{
-    //    //var modelEndpoint = "/engines/text-davinci-002/completions";
-    //    var modelEndpoint = "/completions";
-    //    var requestBody = new
-    //    {
-    //        prompt = "Write a program in C# to find the largest number in an array",
-    //        max_tokens = 60,
-    //        n = 1,
-    //        stop = "\n",
-    //        model = "text-davinci-003"
-    //    };
+    [HttpGet(Name = "GetCodeDavinci003")]
+    public async Task<string> GetCodeDavinci003()
+    {
+       //var modelEndpoint = "/engines/text-davinci-002/completions";
+       var modelEndpoint = "/completions";
+       var requestBody = new
+       {
+           prompt = "Write a program in C# to find the largest number in an array",
+           max_tokens = 60,
+           n = 1,
+           stop = "\n",
+           model = "text-davinci-003"
+       };
 
-    //    var response = await SendCompletionRequest(modelEndpoint, requestBody);
-    //    var choices = response?.choices;
-    //    var completionText = choices?[0]?.text;
+       var response = await SendCompletionRequest(modelEndpoint, requestBody);
+       var choices = response?.choices;
+       var completionText = choices?[0]?.text;
 
-    //    return completionText;
-    //}
-
-    //[HttpGet(Name = "GetCodeDavinci003")]
-    //public async Task<string> GetCodeDavinci003()
-    //{
-    //    //var modelEndpoint = "/engines/text-davinci-002/completions";
-    //    var modelEndpoint = "/completions";
-    //    var requestBody = new
-    //    {
-    //        prompt = "Write a program in C# to find the largest number in an array",
-    //        max_tokens = 60,
-    //        n = 1,
-    //        stop = "\n",
-    //        model = "text-davinci-003"
-    //    };
-
-    //    var response = await SendCompletionRequest(modelEndpoint, requestBody);
-    //    var choices = response?.choices;
-    //    var completionText = choices?[0]?.text;
-
-    //    return completionText;
-    //}
+       return completionText;
+    }
 
     private async Task<dynamic> SendCompletionRequest(string modelEndpoint, dynamic requestBody)
     {
