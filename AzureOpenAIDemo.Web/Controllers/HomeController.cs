687cc08fa7dd72b994fa41bb041ab4344249aaf5
Randy Pagels <rpagels@microsoft.com>
2023-03-01 09:10:21 -0500

    Updates

--- AzureOpenAIDemo.Web/Controllers/HomeController.cs
+++ AzureOpenAIDemo.Web/Controllers/HomeController.cs
@@ -1,32 +1,165 @@
 ﻿using AzureOpenAIDemo.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Configuration;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
 
-namespace AzureOpenAIDemo.Web.Controllers
+namespace AzureOpenAIDemo.Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+    private const string myOpenAPI_URL = "https://api.openai.com/v1/completions";
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+        _configuration = configuration;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public IActionResult Index()
+    {
+        // Save App Configuration Dynamic Configuration Settings
+        ViewData["myOpenAPI_Key"] = _configuration["OPENAPI_KEY"];
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public async Task<ActionResult> generateOpenAPI(string prompt)
+    {
+        try
         {
-            _logger = logger;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration["OPENAPI_KEY"]);
+
+                var json = JsonConvert.SerializeObject(new
+                {
+                    prompt = prompt,
+                    model = "text-davinci-003",
+                    max_tokens = 100,
+                    temperature = 0.5,
+                    top_p = 1,
+                    frequency_penalty = 0,
+                    presence_penalty = 0
+                });
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
+                var response = await client.PostAsync(myOpenAPI_URL, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseBody);
+
+                    var choices = responseObject["choices"];
+                    if (choices != null && choices.Any())
+                    {
+                        var generatedText = (string)choices[0]!["text"]!;
+                        return Json(new { success = true, generatedText = generatedText });
+                    }
+                }
+
+                return Json(new { success = false, errorMessage = "Failed to generate text." });
+            }
         }
-
-        public IActionResult Index()
+        catch (Exception ex)
         {
-            return View();
+            return Json(new { success = false, errorMessage = ex.Message });
         }
+    }
 
-        public IActionResult Privacy()
+    public async Task<ActionResult> generateAPIM(string prompt)
+    {
+        try
         {
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration["OPENAPI_KEY"]);
+
+                var json = JsonConvert.SerializeObject(new
+                {
+                    prompt = prompt,
+                    model = "text-davinci-003",
+                    max_tokens = 100,
+                    temperature = 0.5,
+                    top_p = 1,
+                    frequency_penalty = 0,
+                    presence_penalty = 0
+                });
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
+                var response = await client.PostAsync(myOpenAPI_URL, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseBody);
+
+                    var choices = responseObject["choices"];
+                    if (choices != null && choices.Any())
+                    {
+                        var generatedText = (string)choices[0]["text"];
+                        return Json(new { success = true, generatedText = generatedText });
+                    }
+                }
+
+                return Json(new { success = false, errorMessage = "Failed to generate text." });
+            }
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        catch (Exception ex)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Json(new { success = false, errorMessage = ex.Message });
+        }
+    }
+
+    // Helper Function
+    private async Task<JObject> SendApiRequestAsync(string prompt)
+    {
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration["OPENAPI_KEY"]);
+
+            var json = JsonConvert.SerializeObject(new
+            {
+                prompt = prompt,
+                model = "text-davinci-003",
+                max_tokens = 100,
+                temperature = 0.5,
+                top_p = 1,
+                frequency_penalty = 0,
+                presence_penalty = 0
+            });
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(myOpenAPI_URL, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var responseObject = JObject.Parse(responseBody);
+                return responseObject;
+            }
+
+            throw new Exception("Failed to generate text.");
         }
     }
 }
--- AzureOpenAIDemo.Web/Controllers/OpenAIController.cs
+++ AzureOpenAIDemo.Web/Controllers/OpenAIController.cs
@@ -1,144 +1,149 @@
-﻿using System.Net.Http.Headers;
-using System.Reflection;
-using System.Text;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿////using System.Net.Http.Headers;
+////using System.Reflection;
+////using System.Text;
+////using Microsoft.AspNetCore.Mvc;
+////using Newtonsoft.Json;
+////using Newtonsoft.Json.Linq;
 
-namespace AzureOpenAIDemo.Web.Controllers;
+////namespace AzureOpenAIDemo.Web.Controllers;
 
-public class OpenAIController : Controller
-{
-    private static string ApimSubscriptionKey = Environment.GetEnvironmentVariable("ApimSubscriptionKey");
-    private static string ApimWebServiceURL = Environment.GetEnvironmentVariable("ApimWebServiceURL");
-    private static string OPENAPI_KEY = Environment.GetEnvironmentVariable("OpenAIAPIKey");
-    private const string API_URL = "https://api.openai.com/v1/completions";
+////public class OpenAIController : Controller
+////{
+////    //private static string ApimSubscriptionKey = Environment.GetEnvironmentVariable("ApimSubscriptionKey");
+////    //private static string ApimWebServiceURL = Environment.GetEnvironmentVariable("ApimWebServiceURL");
+////    ////private static string OPENAPI_KEY = Environment.GetEnvironmentVariable("OpenAIAPIKey");
+////    //private const string API_URL = "https://api.openai.com/v1/completions";
 
-    public ActionResult Index()
-    {
-        return View();
-    }
+////    //private static string OPENAPI_KEY = "AddKeyHere";
 
-    public async Task<ActionResult> generateOpenAPI(string prompt)
-    {
-        try
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
+////    public ActionResult Index()
+////    {
+////        // Save App Configuration Dynamic Configuration Settings
+////        //ViewData["myOpenAPI_Key"] = OPENAPI_KEY;
 
-                var json = JsonConvert.SerializeObject(new
-                {
-                    prompt = prompt,
-                    model = "text-davinci-003",
-                    max_tokens = 100,
-                    temperature = 0.5,
-                    top_p = 1,
-                    frequency_penalty = 0,
-                    presence_penalty = 0
-                });
+////        return View();
+////    }
 
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
-                var response = await client.PostAsync(API_URL, content);
+////    public async Task<ActionResult> generateOpenAPI(string prompt)
+////    {
+////        try
+////        {
+////            using (var client = new HttpClient())
+////            {
+////                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseBody = await response.Content.ReadAsStringAsync();
-                    var responseObject = JObject.Parse(responseBody);
+////                var json = JsonConvert.SerializeObject(new
+////                {
+////                    prompt = prompt,
+////                    model = "text-davinci-003",
+////                    max_tokens = 100,
+////                    temperature = 0.5,
+////                    top_p = 1,
+////                    frequency_penalty = 0,
+////                    presence_penalty = 0
+////                });
 
-                    var choices = responseObject["choices"];
-                    if (choices != null && choices.Any())
-                    {
-                        var generatedText = (string)choices[0]["text"];
-                        return Json(new { success = true, generatedText = generatedText });
-                    }
-                }
+////                var content = new StringContent(json, Encoding.UTF8, "application/json");
+////                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
+////                var response = await client.PostAsync(API_URL, content);
 
-                return Json(new { success = false, errorMessage = "Failed to generate text." });
-            }
-        }
-        catch (Exception ex)
-        {
-            return Json(new { success = false, errorMessage = ex.Message });
-        }
-    }
+////                if (response.IsSuccessStatusCode)
+////                {
+////                    var responseBody = await response.Content.ReadAsStringAsync();
+////                    var responseObject = JObject.Parse(responseBody);
 
-    public async Task<ActionResult> generateAPIM(string prompt)
-    {
-        try
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
+////                    var choices = responseObject["choices"];
+////                    if (choices != null && choices.Any())
+////                    {
+////                        var generatedText = (string)choices[0]["text"];
+////                        return Json(new { success = true, generatedText = generatedText });
+////                    }
+////                }
 
-                var json = JsonConvert.SerializeObject(new
-                {
-                    prompt = prompt,
-                    model = "text-davinci-003",
-                    max_tokens = 100,
-                    temperature = 0.5,
-                    top_p = 1,
-                    frequency_penalty = 0,
-                    presence_penalty = 0
-                });
+////                return Json(new { success = false, errorMessage = "Failed to generate text." });
+////            }
+////        }
+////        catch (Exception ex)
+////        {
+////            return Json(new { success = false, errorMessage = ex.Message });
+////        }
+////    }
 
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
-                var response = await client.PostAsync(API_URL, content);
+////    public async Task<ActionResult> generateAPIM(string prompt)
+////    {
+////        try
+////        {
+////            using (var client = new HttpClient())
+////            {
+////                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseBody = await response.Content.ReadAsStringAsync();
-                    var responseObject = JObject.Parse(responseBody);
+////                var json = JsonConvert.SerializeObject(new
+////                {
+////                    prompt = prompt,
+////                    model = "text-davinci-003",
+////                    max_tokens = 100,
+////                    temperature = 0.5,
+////                    top_p = 1,
+////                    frequency_penalty = 0,
+////                    presence_penalty = 0
+////                });
 
-                    var choices = responseObject["choices"];
-                    if (choices != null && choices.Any())
-                    {
-                        var generatedText = (string)choices[0]["text"];
-                        return Json(new { success = true, generatedText = generatedText });
-                    }
-                }
+////                var content = new StringContent(json, Encoding.UTF8, "application/json");
+////                //var response = await client.PostAsync(API_URL, new StringContent(json.ToString()));
+////                var response = await client.PostAsync(API_URL, content);
 
-                return Json(new { success = false, errorMessage = "Failed to generate text." });
-            }
-        }
-        catch (Exception ex)
-        {
-            return Json(new { success = false, errorMessage = ex.Message });
-        }
-    }
+////                if (response.IsSuccessStatusCode)
+////                {
+////                    var responseBody = await response.Content.ReadAsStringAsync();
+////                    var responseObject = JObject.Parse(responseBody);
 
-    // Helper Function
-    private async Task<JObject> SendApiRequestAsync(string prompt)
-    {
-        using (var client = new HttpClient())
-        {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
+////                    var choices = responseObject["choices"];
+////                    if (choices != null && choices.Any())
+////                    {
+////                        var generatedText = (string)choices[0]["text"];
+////                        return Json(new { success = true, generatedText = generatedText });
+////                    }
+////                }
 
-            var json = JsonConvert.SerializeObject(new
-            {
-                prompt = prompt,
-                model = "text-davinci-003",
-                max_tokens = 100,
-                temperature = 0.5,
-                top_p = 1,
-                frequency_penalty = 0,
-                presence_penalty = 0
-            });
+////                return Json(new { success = false, errorMessage = "Failed to generate text." });
+////            }
+////        }
+////        catch (Exception ex)
+////        {
+////            return Json(new { success = false, errorMessage = ex.Message });
+////        }
+////    }
 
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(API_URL, content);
+////    // Helper Function
+////    private async Task<JObject> SendApiRequestAsync(string prompt)
+////    {
+////        using (var client = new HttpClient())
+////        {
+////            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OPENAPI_KEY);
 
-            if (response.IsSuccessStatusCode)
-            {
-                var responseBody = await response.Content.ReadAsStringAsync();
-                var responseObject = JObject.Parse(responseBody);
-                return responseObject;
-            }
+////            var json = JsonConvert.SerializeObject(new
+////            {
+////                prompt = prompt,
+////                model = "text-davinci-003",
+////                max_tokens = 100,
+////                temperature = 0.5,
+////                top_p = 1,
+////                frequency_penalty = 0,
+////                presence_penalty = 0
+////            });
 
-            throw new Exception("Failed to generate text.");
-        }
-    }
+////            var content = new StringContent(json, Encoding.UTF8, "application/json");
+////            var response = await client.PostAsync(API_URL, content);
 
-}
+////            if (response.IsSuccessStatusCode)
+////            {
+////                var responseBody = await response.Content.ReadAsStringAsync();
+////                var responseObject = JObject.Parse(responseBody);
+////                return responseObject;
+////            }
+
+////            throw new Exception("Failed to generate text.");
+////        }
+////    }
+
+////}
